edafa6b2fe8f261fe222403f5776a8b7980dfe35
이국현 <125985700+2gukhyeon@users.noreply.github.com>
2026-03-27 09:58:39 +0900

    Update reward_fns.py

--- reward_fns.py
+++ reward_fns.py
@@ -173,7 +173,7 @@ def brier_reward(format_pattern,completions,answer,source=None, **kwargs):
             
             if format_pattern == "tabc_align": # aligning reasoning and answer
                 if cr > 0.5: # correct case
-                    align_weight = 0.2
+                    align_weight = 0.3
                     align_reward = align_weight * (conf - float(reasoning_conf))**2
                     reward = 1 - brier - align_reward
                 else: # incorrect case
