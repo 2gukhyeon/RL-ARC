29a7be3d3a03120d22e71cebeb6fc63136a55853
이국현 <125985700+2gukhyeon@users.noreply.github.com>
2026-05-23 20:31:04 +0900

    Add files via upload

---
+++ dataset_rl_arc.py
@@ -0,0 +1,85 @@
+from torch.utils.data import Dataset
+from preprocess import preprocess
+from transformers import AutoTokenizer
+import torch 
+import pandas as pd
+import json
+from datasets import load_dataset
+from utils import duplicate
+# input format
+
+instruction = "\n\nPROBLEM: {question}\n\nEND OF PROBLEM\n\nMODEL'S RESPONSE: {response}\n\nEND OF RESPONSE\n\n"
+
+
+IGNORE_INDEX: int = -100
+
+
+# For cls
+class CorrectionJSONDataset(Dataset):
+    def __init__(self, q_path, tokenizer, is_train=True,): 
+        self.q_path = q_path
+        self.data = []
+        self.tokenizer = tokenizer
+        self.is_train = is_train
+
+
+
+        with open(self.q_path, "r", encoding="utf-8") as f:
+            self.data = json.load(f)
+
+        self.answers = [line["is_correct"] for line in self.data]
+        if is_train:
+            self.prepare_sft_dataset()
+        else:
+            self.validation()
+
+
+    def tokenizing(self, lines):
+        encoding = self.tokenizer(
+            lines,
+            return_tensors = "pt",
+            # add_special_tokens=True,
+            padding=True,
+            truncation=True,
+            max_length=4096 # ours
+        )
+        
+        return encoding
+    
+    
+    def prepare_sft_dataset(self, verbose=True):
+        prompts = [instruction.format(question=item["question"], response=item["response"]) for item in self.data]
+
+
+        self.labels = [int(item) for item in self.answers]
+        self.encoding = self.tokenizing(prompts)
+        
+        # torch.set_printoptions(profile="full") # for code verifying
+        if verbose:
+            print(f'sample example: {prompts[0]}')
+            print(self.labels[0])
+
+        return None
+    
+    def validation(self, verbose=False):
+        # inputs
+        self.inputs = [instruction.format(question=item["question"], response=item["response"]) for item in self.data]
+        # labels
+        self.labels = [int(item) for item in self.answers]
+        
+        if verbose:
+            print(self.inputs[0])
+            print(self.labels[0])
+            
+        return None
+    
+    def __getitem__(self, index):
+        return {
+            "input_ids": self.encoding["input_ids"][index],
+            "attention_mask": self.encoding["attention_mask"][index],
+            "labels": torch.tensor(self.labels[index], dtype=torch.long)
+        }
+
+    def __len__(self):
+        return len(self.labels)  
+        
---
+++ fine_tune_rl-arc.py
@@ -0,0 +1,108 @@
+from transformers import DataCollatorWithPadding, DataCollatorForSeq2Seq
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments, LlamaForSequenceClassification, LlamaConfig, T5ForConditionalGeneration
+from huggingface_hub import login
+import argparse
+import torch
+from dataset_rl_arc import CorrectionJSONDataset
+
+from peft import LoraConfig, get_peft_model, TaskType
+import utils
+from baseline_dataset import BaselineDataset
+# for utilizing GPU
+device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
+
+#### argument #### 
+parser = argparse.ArgumentParser()
+# required = True
+parser.add_argument('--model_ckpt', help='pre-trained open-source model: huggingface_model_path', type=str, required=True, default="google/gemma-2b")
+parser.add_argument('--seed', help='seed (2, 10, or 42)', type=int, required=True)
+parser.add_argument('--llm', help='llama3.2, gpt3.5, llama2, mistral', type=str, required=True)
+
+# required = False
+parser.add_argument('--ratio', help='reasoning thres.', type=float, required=False, default=-1)
+parser.add_argument('--per_device_train_batch_size', help='total batch size / # of gradient accumulation steps', type=int, required=False, default=16)
+parser.add_argument('--gradient_accumulation_steps', help='# of gradient accumulation steps', type=int, required=False, default=1)
+parser.add_argument('--save_path', help='path where the aligner model ckpt to be saved', type=str, required=False, default='./models')
+parser.add_argument('--logging_dir', help='path where the logging of aligner model to be saved', type=str, required=False, default="./runs")
+parser.add_argument('--lr', help='learning rate', type=float, required=False, default=2e-4)
+parser.add_argument('--lr_scheduler_type', help='learning rate scheduler', type=str, required=False, default="cosine")
+parser.add_argument('--epoch', help='training epoch', type=int, required=False, default=6)
+parser.add_argument('--lr_warmup_ratio', help='warmup step ratio, which is # of steps ("total steps * ratio")', type=float, required=False, default=0) # llama
+parser.add_argument('--weight_decay', help='weight decay', type=float, required=False, default=0.0)
+parser.add_argument('--huggingface_api_key', help='huggingface api key for gemma, llama ...', type=str, required=False, default="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")
+args = parser.parse_args()
+
+
+seed = args.seed
+llm = args.llm
+model_ckpt = args.model_ckpt
+model_name = model_ckpt.split("/")[1]
+lr = args.lr
+lr_scheduler_type = args.lr_scheduler_type
+epoch = args.epoch
+per_device_train_batch_size = args.per_device_train_batch_size
+gradient_accumulation_steps = args.gradient_accumulation_steps
+lr_warmup_ratio = args.lr_warmup_ratio
+weight_decay = args.weight_decay
+
+
+num_labels = 2
+question_path = "./dataset/piqa/train.jsonl"
+
+save_path = f"{args.save_path}/{llm}/{task}/cls/{model_name}/{is_ours}/lora_{is_lora}/{seed}/{back_ratio}"
+logging_dir = f"{args.logging_dir}/{llm}/{task}/cls/{model_name}/{is_ours}/lora_{is_lora}/{seed}/{back_ratio}"
+
+
+##############################################################
+################# main code ##################################
+##############################################################
+api_key = args.huggingface_api_key
+login(token=api_key)
+
+tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
+# eos랑 pad를 같게 해선 안된다. eos에 대한 로스가 사라져, 문장이 그냥 길어짐.
+tokenizer.padding_side = "right" # standard methods
+
+### main model ###f
+base_model = AutoModelForSequenceClassification.from_pretrained(model_ckpt, num_labels=num_labels)
+base_model.resize_token_embeddings(len(tokenizer))
+base_model.config.pad_token_id = tokenizer.pad_token_id
+
+
+
+dataset = ClassifierDataset(question_path, tokenizer, True) # OURS
+# dataset = BaselineDataset(task, question_path, answer_path, tokenizer,True, None, "gkp") # baseline
+data_collator = DataCollatorWithPadding(tokenizer, pad_to_multiple_of=8)
+print("### loaded dataset ###")
+
+training_args = TrainingArguments(
+    output_dir=save_path,
+    logging_strategy='steps',
+    logging_steps=50,
+    torch_compile=True,
+    save_strategy="epoch",
+    num_train_epochs=epoch,
+    per_device_train_batch_size=per_device_train_batch_size,
+    gradient_accumulation_steps=gradient_accumulation_steps,
+    learning_rate=lr,
+    lr_scheduler_type=lr_scheduler_type,
+    warmup_ratio=lr_warmup_ratio,
+    weight_decay=weight_decay,
+    seed=seed,
+    report_to='tensorboard',
+    logging_dir=logging_dir,
+    gradient_checkpointing=False
+)
+
+trainer = Trainer(
+    model=base_model.to(device),
+    args=training_args,
+    train_dataset=dataset,
+    tokenizer=tokenizer,
+    data_collator=data_collator
+)
+
+print('### start fine-tuning ###')
+base_model.config.use_cache=False
+trainer.train()
+print('### ended fine-tuning ###')
